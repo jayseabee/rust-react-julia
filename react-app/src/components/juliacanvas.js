643c9a3a5886f76fc17b99abfe4daa749ed1fac7
jayseabee <jayseabee@protonmail.com>
2023-03-06 18:40:15 +0800

    fixed warning on string check

--- react-app/src/components/juliacanvas.js
+++ react-app/src/components/juliacanvas.js
@@ -3,7 +3,7 @@ import GetCanvas from "./getcanvas";
 import WsCanvas from "./wscanvas";
 
 function JuliaCanvas(props) {
-  const useSocket = props.api == "websocket";
+  const useSocket = props.api === "websocket";
   return (
     <div className="App-body">
       {useSocket ? <WsCanvas {...props} /> : <GetCanvas {...props} />}
